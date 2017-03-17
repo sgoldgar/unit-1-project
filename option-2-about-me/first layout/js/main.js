5a40bf3ef0deb29b9ba14c0d9ee79f717b5dae33
Sarah Goldgar <sgoldgar@gmail.com>
2017-03-17 13:11:23 -0500

    original idea for layout

---
+++ option-2-about-me/first layout/js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+// Activate Carousel
+$("#myCarousel").carousel();
+
+// Enable Carousel Indicators
+$(".item").click(function(){
+    $("#myCarousel").carousel(1);
+});
+
+// Enable Carousel Controls
+$(".left").click(function(){
+    $("#myCarousel").carousel("prev");
+});
+$(".right").click(function(){
+    $("#myCarousel").carousel("next");
+});
+
+
+})
