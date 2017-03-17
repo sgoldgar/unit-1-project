0d7ee9018a5b88b5a275e7fab495f0f71e010c21
Sarah Goldgar <sgoldgar@gmail.com>
2017-03-17 14:18:54 -0500

    added sticky nav and jump scroll

--- option-2-about-me/js/main.js
+++ option-2-about-me/js/main.js
@@ -26,4 +26,46 @@ $('.my-logo').hover(
   }
 );
 
+/*Scroll to Sections*/
+  /*Scroll to top of page when click logo*/
+  $('.my-logo').on('click', function(){
+    $('html,body').scrollTop(0);
+    console.log('top');
+  });
+  /*Scroll to about when click on nav link*/
+$('nav ul li:first-child').first().on('click', function(){
+  $('html, body').animate({
+    scrollTop: $(".about").offset().top -150
+}, 1000);
+  console.log('about');
+});
+  /*Scroll to about when click on carousel link*/
+$('.carousel-inner .item h3 a').on('click', function(){
+  $('html, body').animate({
+    scrollTop: $(".about").offset().top -150
+}, 1000);
+  console.log('about');
+});
+  /*Scroll to projects when click on carousel link*/
+$('.carousel-inner .item h3 a').eq(2).on('click', function(){
+  $('html, body').animate({
+    scrollTop: $(".projects").offset().top -150
+}, 1000);
+  console.log('projects');
+});
+    /*Scroll to projects when click on nav link*/
+$('nav ul li').eq(2).first().on('click', function(){
+  $('html, body').animate({
+    scrollTop: $(".projects").offset().top -150
+}, 1000);
+  console.log('projects');
+});
+    /*Scroll to contact when click on nav link*/
+$('nav ul li').eq(3).first().on('click', function(){
+  $('html, body').animate({
+    scrollTop: $(".contact").offset().top -150
+}, 1000);
+  console.log('contact');
+});
+
 })
